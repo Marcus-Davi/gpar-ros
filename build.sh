60775257f9a11164b66063a9e583343203164e4a
LaptopMarcus <gpardee@dee.ufc.br>
2019-09-11 14:55:29 -0300

    Build for Eclipse

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-catkin_make_isolated -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+catkin_make_isolated -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --force-cmake -G"Eclipse CDT4 - Unix Makefiles"
