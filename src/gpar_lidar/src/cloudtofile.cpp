332a286442c62f7dc33beea74be7cf5e31fd99eb
Marcus Forte <davi2812@dee.ufc.br>
2021-03-18 03:17:49 -0300

    loadscan gazebo

--- src/gpar_lidar/src/cloudtofile.cpp
+++ src/gpar_lidar/src/cloudtofile.cpp
@@ -3,7 +3,7 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/io/pcd_io.h>
-
+#include <pcl/io/ply_io.h>
 #include <tf/transform_listener.h>
 #include <pcl_ros/transforms.h>
 
@@ -15,6 +15,8 @@ std::string reference;
 
 std::string save_path;
 
+bool is_ply = false;
+
 static sensor_msgs::PointCloud2::Ptr cloud_g = boost::make_shared<sensor_msgs::PointCloud2>();
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloudT;
 
@@ -23,11 +25,12 @@ void cloud_callback(const sensor_msgs::PointCloud2::Ptr cloud)
 {
   ROS_INFO("GOT CLOUD!");
 
-  if(got_cloud == false){
-		  ROS_INFO("N Points: %d",cloud->height*cloud->width);
-		  ROS_INFO("N Fields %ld",cloud->fields.size()); 
+  if (got_cloud == false)
+  {
+    ROS_INFO("N Points: %d", cloud->height * cloud->width);
+    ROS_INFO("N Fields %ld", cloud->fields.size());
 
-		  got_cloud = true;
+    got_cloud = true;
   }
 
   cloud_g = cloud;
@@ -36,15 +39,18 @@ void cloud_callback(const sensor_msgs::PointCloud2::Ptr cloud)
 bool save_map(std_srvs::TriggerRequest &req, std_srvs::TriggerResponse &resp)
 {
   static pcl::PCLPointCloud2::Ptr pcl_cloud = pcl::make_shared<pcl::PCLPointCloud2>();
-	// pcl::io::savePCDFile("teste.pcd",*cloud2);
+  // pcl::io::savePCDFile("teste.pcd",*cloud2);
 
   std::string ref_;
-  if(has_reference_param) {
+  if (has_reference_param)
+  {
     ref_ = reference;
-  } else {
+  }
+  else
+  {
     ref_ = cloud_g->header.frame_id;
   }
-  
+
   try
   {
     if (!pcl_ros::transformPointCloud(ref_, *cloud_g, *cloud_g, *tf_))
@@ -61,20 +67,31 @@ bool save_map(std_srvs::TriggerRequest &req, std_srvs::TriggerResponse &resp)
     return false;
   }
   // Stamp
-  pcl_conversions::moveToPCL(*cloud_g,*pcl_cloud);
+  pcl_conversions::moveToPCL(*cloud_g, *pcl_cloud);
   std::time_t std_time;
   char *time_date = std::ctime(&std_time);
   ros::Time time = ros::Time::now();
   std_time = time.sec;
   std::string str_time = std::to_string(time.sec);
-  std::string str_pcd = save_path + "/points_sweep_" + str_time + ".pcd";
+  std::string str_filename = save_path + "/points_sweep_" + str_time;
+  ;
   // pcl::io::savePCDFileBinary(str_pcd,*cloud);
 
   // TODO Verificar exceptions
-  pcl::io::savePCDFile(str_pcd, *pcl_cloud); // May be binary
-  ROS_INFO("PCD Saved at %s", str_pcd.c_str());
+  if (!is_ply)
+  {
+    str_filename += ".pcd";
+    pcl::io::savePCDFile(str_filename, *pcl_cloud); // May be binary
+    ROS_INFO("PCD Saved at %s", str_filename.c_str());
+  }
+  else
+  {
+    str_filename += ".ply";
+    pcl::io::savePLYFile(str_filename, *pcl_cloud);
+    ROS_INFO("PLY Saved at %s", str_filename.c_str());
+  }
   resp.success = true;
-  resp.message = "saved at " + str_pcd;
+  resp.message = "saved at " + str_filename;
   return true;
 }
 
@@ -108,11 +125,35 @@ int main(int argc, char **argv)
 
   std::string cloud_topic = n.resolveName("cloud");
 
-  if(! nh.getParam("reference", reference) ){
+  std::string file_format;
+
+  nh.param<std::string>("file_format", file_format, "pcd");
+
+  if (file_format == "pcd")
+  {
+    is_ply = false;
+  }
+  else if (file_format == "ply")
+  {
+    is_ply = true;
+  }
+  else
+  {
+    ROS_ERROR("File Format not supported.");
+    ros::shutdown();
+    exit(-1);
+  }
+
+  ROS_WARN("File format: %s", file_format.c_str());
+
+  if (!nh.getParam("reference", reference))
+  {
     has_reference_param = false;
     ROS_WARN("Using laser frame of reference");
-  } else {
-    ROS_WARN("Using reference -> %s",reference.c_str());
+  }
+  else
+  {
+    ROS_WARN("Using reference -> %s", reference.c_str());
   }
 
   //set save folder
@@ -130,7 +171,7 @@ int main(int argc, char **argv)
 
   ros::ServiceServer save_service = nh.advertiseService("save_cloud", save_map);
   ros::Subscriber sub_cloud = n.subscribe(cloud_topic, 10, cloud_callback);
-  
+
   ROS_WARN("subscribed to cloud topic %s. use service ~/save_cloud to save the point cloud", cloud_topic.c_str());
   ROS_WARN("Clouds are saved at %s", save_path.c_str());
 
